517e4034df34ab3b3ca4e993a6acaa557e665ad7
kimtaehyun <kimtaehyun98@naver.com>
2022-05-13 21:01:22 +0900

    JPA 설정 제거

--- src/main/java/hackerton/server/Model/Member.java
+++ src/main/java/hackerton/server/Model/Member.java
@@ -1,12 +1,7 @@
 package hackerton.server.Model;
 
-import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
--- src/main/java/hackerton/server/Repository/TestRepository.java
+++ src/main/java/hackerton/server/Repository/TestRepository.java
@@ -1,10 +1,7 @@
 package hackerton.server.Repository;
 
-import hackerton.server.Model.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
 
 @Repository
 @RequiredArgsConstructor
