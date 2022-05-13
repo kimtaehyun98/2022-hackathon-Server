e7062791ccb73071a6c46a5c27c71d093c694f7e
kimtaehyun <kimtaehyun98@naver.com>
2022-05-14 06:50:09 +0900

    Cross Origin 변경

--- src/main/java/hackerton/server/Controller/ProblemController.java
+++ src/main/java/hackerton/server/Controller/ProblemController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-@CrossOrigin(origins = "http://d3mri5dv71y0ua.cloudfront.net/")
+@CrossOrigin(origins = {"http://localhost:3000", "http://d3mri5dv71y0ua.cloudfront.net/"})
 public class ProblemController {
 
 	private final ProblemRepository problemRepository;
